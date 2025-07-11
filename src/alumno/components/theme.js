e63c8a9e935331d10a695da9381834733a0b988b
DanielRa82 <110646373+DanielRa82@users.noreply.github.com>
2025-07-11 15:33:30 -0500

    AÑADIR TEMAS

---
+++ src/alumno/components/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#f3f7ff',
+      paper: '#ffffff',
+    },
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#121212',
+      paper: '#1e1e1e',
+    },
+  },
+});
