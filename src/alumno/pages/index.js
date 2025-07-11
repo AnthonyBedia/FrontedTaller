50c90e42d1f9212105e68b61ffeda6016535d210
DanielRa82 <110646373+DanielRa82@users.noreply.github.com>
2025-07-11 15:39:36 -0500

    Update index.js

--- src/alumno/pages/index.js
+++ src/alumno/pages/index.js
@@ -3,6 +3,6 @@ export * from './Registro';
 export * from './MisCursos';
 export * from './Calificaciones';
 export * from './Horarios';
-export * from './Perfil';
+export * from './Personalizacion';
 export * from './Mensajes';
 export * from './Biblioteca';
